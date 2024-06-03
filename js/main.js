ed7ca015b5ba084f19bd5220ef6be323628d21ae
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-03 23:37:27 +0530

    1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// modules more practice
+// importing all with changing name
+import * as userData from './userData.js';
+
+console.log(userData.user1());
+console.log(userData.user2());
+console.log(userData.user3());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
